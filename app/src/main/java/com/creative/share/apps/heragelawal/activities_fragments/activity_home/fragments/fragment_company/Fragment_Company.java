dc68b919eb5375e278fafdf3a07bad7ddb048655
ARAB <ahmedmohamed23345@gmail.com>
2019-09-30 16:23:40 +0200

    ch

--- app/src/main/java/com/creative/share/apps/heragelawal/activities_fragments/activity_home/fragments/fragment_company/Fragment_Company.java
+++ app/src/main/java/com/creative/share/apps/heragelawal/activities_fragments/activity_home/fragments/fragment_company/Fragment_Company.java
@@ -11,13 +11,21 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.creative.share.apps.heragelawal.R;
 import com.creative.share.apps.heragelawal.activities_fragments.activity_home.activity.HomeActivity;
+import com.creative.share.apps.heragelawal.adapter.Companies_Adapter;
+import com.creative.share.apps.heragelawal.adapter.Company_Ads_Type_Adapter;
 import com.creative.share.apps.heragelawal.databinding.FragmentCompanyBinding;
+import com.creative.share.apps.heragelawal.models.Catohries_Model;
 import com.creative.share.apps.heragelawal.models.UserModel;
 import com.creative.share.apps.heragelawal.preferences.Preferences;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import io.paperdb.Paper;
@@ -28,7 +36,10 @@ public class Fragment_Company extends Fragment {
     private Preferences preferences;
     private UserModel userModel;
     private String lang;
-
+    private Companies_Adapter side_catogry_adapter;
+    private Company_Ads_Type_Adapter company_ads_type_adapter;
+    private List<Catohries_Model> catohries_modelList;
+private LinearLayoutManager manager;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_company, container, false);
@@ -39,7 +50,7 @@ public class Fragment_Company extends Fragment {
     }
 
     private void initView() {
-
+catohries_modelList=new ArrayList<>();
         activity = (HomeActivity) getActivity();
         preferences = Preferences.newInstance();
         userModel = preferences.getUserData(activity);
@@ -49,6 +60,16 @@ public class Fragment_Company extends Fragment {
 
         binding.progBar2.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
         binding.progBar3.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+
+        side_catogry_adapter = new Companies_Adapter(catohries_modelList, activity);
+        company_ads_type_adapter = new Company_Ads_Type_Adapter(catohries_modelList, activity);
+
+        binding.recStores.setLayoutManager(new GridLayoutManager(activity, 3));
+        binding.recStores.setAdapter(side_catogry_adapter);
+        manager=new GridLayoutManager(activity,2,GridLayoutManager.HORIZONTAL,false);
+        binding.recType.setLayoutManager(manager);
+        binding.recType.setAdapter(company_ads_type_adapter);
+        adddatat();
         binding.tvAds.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,6 +79,37 @@ public class Fragment_Company extends Fragment {
 
 
     }
+    private void adddatat() {
+
+        List<Catohries_Model.Order_details> order_details = new ArrayList<>();
+        order_details.add(new Catohries_Model.Order_details());
+        order_details.add(new Catohries_Model.Order_details());
+        order_details.add(new Catohries_Model.Order_details());
+
+        Catohries_Model catohries_model = new Catohries_Model();
+        catohries_model.setOrder_details(order_details);
+
+        Catohries_Model catohries_model1 = new Catohries_Model();
+        catohries_model1.setOrder_details(order_details);
+
+        Catohries_Model catohries_model2 = new Catohries_Model();
+        catohries_model2.setOrder_details(order_details);
+        catohries_modelList.add(catohries_model);
+        catohries_modelList.add(catohries_model1);
+        catohries_modelList.add(catohries_model2);
+        catohries_modelList.add(catohries_model);
+        catohries_modelList.add(catohries_model1);
+        catohries_modelList.add(catohries_model2);
+        catohries_modelList.add(catohries_model);
+        catohries_modelList.add(catohries_model1);
+        catohries_modelList.add(catohries_model2);
+        binding.progBar2.setVisibility(View.GONE);
+        binding.progBar3.setVisibility(View.GONE);
+        side_catogry_adapter.notifyDataSetChanged();
+        company_ads_type_adapter.notifyDataSetChanged();
+
+    }
+
 
 
     public static Fragment_Company newInstance() {
